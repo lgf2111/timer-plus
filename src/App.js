d47bda62f82f6c2f6a2ee10fc15ec13fd5291fb5
lgf2111 <lgf2111@gmail.com>
2022-08-20 01:28:08 +0800

    moved timer into app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React, {useState, useRef} from 'react'
+import { useTimer } from 'react-timer-hook';
 
-import Timer from './Timer'
 import OptionList from './OptionList'
 
 function App() {
-  // useState & useRef
   const [expiryTimestamp, setExpiryTimestamp] = useState(0)
+
+  const divRef = useRef()
 
   const options =  {hour:   {ref: useRef(), values: [...Array(25).keys()]},
                     minute: {ref: useRef(), values: [...Array(60).keys()]},
                     second: {ref: useRef(), values: [...Array(60).keys()]}}
+    
+  const {
+    seconds, minutes, hours, days,
+    isRunning,
+    start, pause, resume, restart,
+  } = useTimer({ expiryTimestamp, autoStart: false, onExpire: () => {
+    let text = "Time's Up!"
+    window.speechSynthesis.speak(new SpeechSynthesisUtterance(text))
+    // TODO: fix expire className
+    divRef.current.className = "timer expired"
+  } });
+  
+  const [hours_, minutes_, seconds_] = [hours, minutes, seconds].map(el => {
+    return el.toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false
+    })
+  })
 
   function handleStartTimer() {
     const hour = parseInt(options.hour.ref.current.value)
@@ -19,14 +38,12 @@ function App() {
     const expiryTimestamp = new Date()
     expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + totalSecond)
     setExpiryTimestamp(expiryTimestamp)
+    restart(expiryTimestamp)
   }
 
   return (
     <>
-    {(() => {
-      if (expiryTimestamp === 0) return <div className='timer idle'><span>00</span>:<span>00</span>:<span>00</span><br/></div>
-      return <Timer expiryTimestamp={expiryTimestamp}/>
-    })()}
+    <div ref={divRef} className={`timer ${isRunning?'active':'idle'}`}><span>{hours_}</span>:<span>{minutes_}</span>:<span>{seconds_}</span><br/></div>
     <OptionList options={options}/>
     <br />
     <button onClick={handleStartTimer}>Start Timer</button>
